f2ed47f2cc61892dfbf7335c73e22e5aa3eac1b2
tpsumeta <tpsumeta@gmail.com>
2020-03-19 15:45:20 +0700

    add okhttp3 acitvity

--- app/src/main/java/io/github/sumeta/android/kotlin/rest/OKHttpActivity.kt
+++ app/src/main/java/io/github/sumeta/android/kotlin/rest/OKHttpActivity.kt
@@ -1,34 +1,25 @@
 package io.github.sumeta.android.kotlin.rest
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
 import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class OKHttpActivity : AppCompatActivity() {
 
     private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_okhttp)
+
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
         println("start app")
 
         run("https://api.github.com/users/sumeta/repos")
-
-        val btnHttp:Button = findViewById(R.id.btnOKHttp)
-
-        intent = Intent(this, OKHttpActivity::class.java)
-
-        btnHttp.setOnClickListener {startActivity(intent)}
-
-
     }
 
     fun run(url: String) {
@@ -41,4 +32,5 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
         })
     }
+
 }
